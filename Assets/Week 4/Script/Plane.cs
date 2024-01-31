b1e122dfa09ef532de61ea263497d7a9c7fb73b1
Jack <Maclean.Mealey@sheridancollage.ca>
2024-01-31 14:39:46 -0500

    Made the plane follow line

--- Assets/Week 4/Script/Plane.cs
+++ Assets/Week 4/Script/Plane.cs
@@ -8,10 +8,47 @@ public class Plane : MonoBehaviour
     public float newPointThreshold = 0.2f;
     Vector2 lastPosition;
     LineRenderer lineRenderer;
+    Vector2 currentPosition;
+    Rigidbody2D RB;
+    public float Speed = 1;
 
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0, transform.position);
+
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        currentPosition = transform.position;
+        if(points.Count > 0)
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            RB.rotation = -angle;
+        }
+        RB.MovePosition(RB.position + (Vector2)transform.up * Speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        if(points.Count > 0)
+        {
+            if (Vector2.Distance(currentPosition, points[0]) < newPointThreshold)
+            {
+                points.RemoveAt(0);
+
+                for (int i = 0; i < lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                }
+                lineRenderer.positionCount--;
+            }
+        }
     }
 
     void OnMouseDown()
@@ -21,7 +58,6 @@ public class Plane : MonoBehaviour
         points.Add(newPosition);
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
-
     }
 
     void OnMouseDrag()

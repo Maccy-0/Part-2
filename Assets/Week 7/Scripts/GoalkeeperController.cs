5816e0f8834cd1a55562ec659c42fef439696f18
Jack <Maclean.Mealey@sheridancollage.ca>
2024-02-21 21:37:52 -0500

    Work in progress

--- Assets/Week 7/Scripts/GoalkeeperController.cs
+++ Assets/Week 7/Scripts/GoalkeeperController.cs
@@ -3,6 +3,7 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class GoalkeeperController : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class GoalkeeperController : MonoBehaviour
     public float distance = 2.5f;
     public Vector2 goalpoint;
     Vector2 placeholder;
+    Vector2 target = new Vector2(0,0);
     
     // Start is called before the first frame update
     void Start()
@@ -18,16 +20,20 @@ public class GoalkeeperController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        //Vector2.MoveTowards(goalKeeper.position, target, 0);
+        Vector2.MoveTowards(goalKeeper.position, Vector2.zero, 1);
+        Debug.Log(goalKeeper.position);
+        
         placeholder = (Vector2)Controller.CurrentSelection.transform.position / Controller.CurrentSelection.transform.position.normalized;
         if (placeholder.x>4)
         {
-            goalKeeper.position = ((Vector2)Controller.CurrentSelection.transform.position).normalized * distance;
+            target = ((Vector2)Controller.CurrentSelection.transform.position).normalized * distance;
         }
         else
         {
-            goalKeeper.position = ((Vector2)Controller.CurrentSelection.transform.position).normalized * placeholder / 2;
+            target = ((Vector2)Controller.CurrentSelection.transform.position).normalized * placeholder / 2;
         }
         
     }
